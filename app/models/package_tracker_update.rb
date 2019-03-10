178836a5469c4e6b792494ba7ac440be38f0ae19
Corey Alexander <coreyja@gmail.com>
2019-03-09 23:18:09 -0500

    Remove Slack webhooks (#282)
    * Remove Slack webhooks
    
    * :cop:

--- app/models/package_tracker_update.rb
+++ app/models/package_tracker_update.rb
@@ -11,10 +11,7 @@ class PackageTrackerUpdate
   def perform!
     package.transaction do
       package.update! tracker.to_package_attrs
-      if send_notification?
-        send_slack_update!
-        send_push_notifications!
-      end
+      send_push_notifications! if send_notification?
       process_tracking_updates!
     end
   end
@@ -27,10 +24,6 @@ class PackageTrackerUpdate
     tracker.tracking_details.each do |tracking_detail|
       TrackingUpdatePerformer.new(package, tracking_detail).save!
     end
-  end
-
-  def send_slack_update!
-    EasypostTrackerSlackPoster.new(package, tracker).post
   end
 
   def send_push_notifications!
