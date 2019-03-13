926f2abc51c57592408bb367ead24f0c9ff8a33b
Corey Alexander <coreyja@gmail.com>
2019-03-12 23:18:16 -0400

    :cop:

--- app/controllers/my/package_archivals_controller.rb
+++ app/controllers/my/package_archivals_controller.rb
@@ -1,11 +1,12 @@
-
 # frozen_string_literal: true
 
-class My::PackageArchivalsController < ApplicationController
-  def create
-    package = current_user.packages.find params[:package_id]
-    package.archive!
+module My
+  class PackageArchivalsController < ApplicationController
+    def create
+      package = current_user.packages.find params[:package_id]
+      package.archive!
 
-    redirect_to my_packages_path
+      redirect_to my_packages_path
+    end
   end
 end
